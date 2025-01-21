d465e52bd8cd4a25e303dac93f58d8c9e649b62c
Astrid Cranney <astrid.cranney@students.snow.edu>
2025-01-21 15:06:53 -0700

    Added LevelUp and DisplayStats methods

--- RPG Battler/Character/Hero.cs
+++ RPG Battler/Character/Hero.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,16 +21,70 @@ namespace RPG_Battler.Character
         public List<Spell> Spells { get; set; }
         public List<Equipment> Equipment { get; set; }
 
-        public Hero()
-        {       
+        public Hero(CombatClass combatClass)
+        {
+            Name = "Unknown";
+            Level = 0;
+            Health = 1;
+            Power = 1;
+            Luck = 1;
+            CombatClass = combatClass;
         }
 
         public void LevelUp()
         {
+            Level += 1;
+            Random LevelUp = new Random();
+            switch (CombatClass)
+            {
+                case CombatClass.Warrior:
+                    {
+                        Health += LevelUp.Next(10, 21);
+                        Power += LevelUp.Next(1, 4);
+                        Luck += LevelUp.Next(1, 4);
+                        break;
+                    }
+                case CombatClass.Wizard:
+                    {
+                        Health += LevelUp.Next(1, 16);
+                        Power += LevelUp.Next(3, 6);
+                        Luck += LevelUp.Next(1, 4);
+                        break;
+                    }
+                case CombatClass.Rogue:
+                    {
+                        Health += LevelUp.Next(1, 16);
+                        Power += LevelUp.Next(1, 4);
+                        Luck += LevelUp.Next(3, 6);
+                        break;
+                    }
+                default:
+                    {
+                        Health += LevelUp.Next(1, 16);
+                        Power += LevelUp.Next(1, 4);
+                        Luck += LevelUp.Next(1, 4);
+                        break;
+                    }
+            }
         }
 
         public void CalculateTotals()
         {
+        }
+
+        public void DisplayStats(bool withEquipment = false)
+        {
+            if (withEquipment)
+            {
+                Console.WriteLine($"Here are {Name} the {CombatClass}'s total stats with equipment on:");
+            }
+            else
+            {
+                Console.WriteLine($"Here are {Name} the {CombatClass}'s natural stats with no equipment on:");
+                Console.WriteLine(@$"Health: {Health}
+Power: {Power}
+Luck: {Luck}");
+            }
         }
     }
 }
